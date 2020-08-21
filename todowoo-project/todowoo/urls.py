c99546d27a2dd93f4b62589f03fec875321cd6b0
marcos <marcoslemeborbafilho@gmail.com>
2020-08-21 18:14:29 -0300

    Review vanilla django on course.

--- todowoo-project/todowoo/urls.py
+++ todowoo-project/todowoo/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', views.signupuser, name='signupuser')
 ]
+
+
+
+
+
